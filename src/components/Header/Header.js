82ea222027a1cb69e00ff93ba33d9450f50b35d1
hasibmiraz <hasibmiraz09@gmail.com>
2022-03-26 01:46:20 +0600

    Header complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="h-15 bg-purple-300">
+      <h1 className="text-center text-3xl">Buyer On Fire</h1>
+    </div>
+  );
+};
+
+export default Header;
